a136ab08b731efc1276d981588c49358cc0cf76b
Michael <bazhanovm@outlook.com>
2025-06-03 22:13:02 -0700

    fix base url

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function AppContent() {
 
 function App() {
   return (
-    <Router basename="/my-pdf-toolbox">
+    <Router basename="/my-pdf-toolbox/">
       <AppContent />
     </Router>
   );
